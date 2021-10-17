e5ebbc4d5b36cfdc487c08a40663ad88c2bcde3f
Oliver Knox <dev@oliverroyknox.com>
2021-10-17 15:45:46 +0100

    MInjected theme handling in html.js to be served before static html, thus prevent flickering per page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./normalize.css";
 import "./typography.css";
 import "./theme.css";
@@ -7,14 +7,5 @@ import "../components/prism-one-dark.scss";
 import "./layout.css";
 
 export default function Layout({children}) {
-    const prefersColorScheme = window?.matchMedia("(prefers-color-scheme: dark)");
-    const [ theme, setTheme ] = useState(prefersColorScheme?.matches ? "dark" : "light");
-
-    prefersColorScheme?.addEventListener("change", e => setTheme(e.matches ? "dark" : "light"));
-
-    useEffect(() => {
-        document.documentElement.className = theme;
-    }, [ theme ]);
-
     return <div>{children}</div>;
 }
---
+++ src/html.js
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        <script
+          key="theme"
+          dangerouslySetInnerHTML={{
+            __html: `(function() { 
+                try { 
+                    const prefersDarkModeQuery = window?.matchMedia("(prefers-color-scheme: dark)");
+
+                    if (prefersDarkModeQuery?.matches) {
+                        document.documentElement.classList.add("dark"); 
+                    } else {
+                        document.documentElement.classList.add("light");
+                    }
+
+                    prefersDarkModeQuery.addEventListener("change", e => setTheme(e.matches))
+
+                    function setTheme(prefersDarkMode) {
+                        document.documentElement.style.transition = "all 300ms ease";
+                        setTimeout(() => {
+                            document.documentElement.style.transition = "";
+                        }, 500);
+
+                        document.documentElement.classList.remove(prefersDarkMode ? "light" : "dark");
+                        document.documentElement.classList.add(prefersDarkMode ? "dark" : "light");
+                    }
+                } catch (e) {} 
+            })();`,
+          }}
+        />
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
