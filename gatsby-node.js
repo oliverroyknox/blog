e34941f30d20ac4688c86f24ad37d794fd1da697
Oliver Knox <oliver.knox@ii.co.uk>
2024-06-12 14:44:27 +0100

    t

---
+++ gatsby-node.js
@@ -0,0 +1,8 @@
+exports.createPages = async ({ actions }) => {
+    const { createRedirect } = actions;
+
+    createRedirect({
+        fromPath: `/product/*/foo`,
+        toPath: `/product/*`,
+    });
+};
